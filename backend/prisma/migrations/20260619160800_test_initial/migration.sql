a5318974fceb38782c45a8b10c47b2b0059c73a9
Adri-LEY <adrien.verstraelen@gmail.com>
2026-06-19 20:34:00 +0100

    Initialisation du projet : Front(React), Back(NestJS) et BDD (PostgresSQL)

---
+++ backend/prisma/migrations/20260619160800_test_initial/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "TestConnexion" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TestConnexion_pkey" PRIMARY KEY ("id")
+);
