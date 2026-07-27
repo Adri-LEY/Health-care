d7c9158c9ea86fe4e77f3a6cc52d72f3d0aaeb83
Adri-LEY <adrien.verstraelen@gmail.com>
2026-07-27 11:23:19 +0100

    feat(front): Ajout de l'affectation des médecins traitants par l'administrateur

--- backend/src/patients/patients.controller.ts
+++ backend/src/patients/patients.controller.ts
@@ -15,7 +15,7 @@ export class PatientsController {
     constructor(private readonly patientsService: PatientsService) {}
 
     @Post('assignDoctor')
-    @Roles('DOCTOR')
+    @Roles('DOCTOR', 'ADMINISTRATOR') // Seuls les médecins et les administrateurs peuvent assigner un médecin à un patient
     @HttpCode(HttpStatus.OK)
     async assignDoctorToPatient(@Body() body: { patientId: number; doctorId: number }) {
         const { patientId, doctorId } = body;
@@ -29,7 +29,7 @@ export class PatientsController {
     }   
 
     @Post('unassignDoctor')
-    @Roles('DOCTOR')
+    @Roles('DOCTOR', 'ADMINISTRATOR') // Seuls les médecins et les administrateurs peuvent désassigner un médecin d'un patient
     @HttpCode(HttpStatus.OK)
     async removeDoctorFromPatient(@Body() body: { patientId: number }, @Req() req) {
         const { patientId } = body;
@@ -47,7 +47,7 @@ export class PatientsController {
     }
 
     @Get('searchPatients')
-    @Roles('DOCTOR', 'NURSE_ASSISTANT')
+    @Roles('DOCTOR', 'NURSE_ASSISTANT', 'ADMINISTRATOR')
     @HttpCode(HttpStatus.OK)
     async searchPatientsByQuery(@Query() dto: SearchPatientsDto) {
         console.log(`Received request to search patients with query "${dto.q}" and limit ${dto.limit}`);
@@ -87,6 +87,21 @@ export class PatientsController {
         const result = await this.patientsService.modifyMedicalRecord(patientId, data);
         return {
             message: `Medical record for patient with ID ${patientId} has been updated`,
+            data: result,
+        };
+    }
+
+
+    @Get(':patientId/profile')
+    @Roles('ADMINISTRATOR')
+    @HttpCode(HttpStatus.OK)
+    async getPatientProfile(
+        @Param('patientId', ParseIntPipe) patientId: number
+    ) {
+        console.log(`Received request to get profile for patient with ID ${patientId}`);
+        const result = await this.patientsService.getPatientProfile(patientId);
+        return {
+            message: `Profile for patient with ID ${patientId}`,
             data: result,
         };
     }
